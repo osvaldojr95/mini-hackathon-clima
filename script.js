292c014766de5d3e9366603e23af0cf4ce46114c
Ana Raquel <anaraquelpmatos@gmail.com>
2022-03-04 20:17:59 -0300

    Reponsividade layout

--- script.js
+++ script.js
@@ -11,10 +11,27 @@ objeto = {
 const link = url_api + `lat=${objeto.lat}&lon=${objeto.lon}&appid=${objeto.appid}&units=${objeto.units}`;
 const promise = axios.get(link);
 
-promise.then((response)=>{
+promise.then((response) => {
     console.log(response);
 })
 
-promise.catch((response)=>{
+promise.catch((response) => {
     console.log(response);
 })
+
+function getLocation() {
+    let latitude = document.querySelector(".latitude").value;
+    let longitude = document.querySelector(".longitude").value;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
